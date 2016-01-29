8390bea7f6f65458e56d9bd9415d20506718e1ea
Olivier Lacan <hi@olivierlacan.com>
2016-01-29 18:02:19 -0500

    Recent builds, Haml views, Dotenv

--- application.rb
+++ application.rb
@@ -1,15 +1,40 @@
 require "bundler"
 Bundler.require
+Dotenv.load
 
 get "/" do
   "Hi, I'm Bob!"
 end
 
+get "/builds" do
+  @recent_builds = builds.map do |build|
+    { number: build.number, state: build.state }
+  end
+
+  haml :builds
+end
+
+def builds
+  @builds ||= repo.builds(after_number: last_build - 25)
+end
+
+def last_build
+  @last_build ||= repo.last_build.number.to_i
+end
+
+def repo
+  @repo ||= travis.repo("#{ENV["GITHUB_ORGANIZATION"]}/#{ENV["GITHUB_REPO"]}")
+end
+
 def travis
-  session = Travis::Pro.session
-  session.api_endpoint = "https://api.travis-ci.com"
-  session.connection = adapter
-  session.github_auth(ENV["GITHUB_API_KEY"])
+  @session ||= begin
+    session = Travis::Pro.session
+    session.connection = connection_adapter
+    session.api_endpoint = "https://api.travis-ci.com"
+    session.github_auth(ENV["GITHUB_API_KEY"])
+
+    session
+  end
 end
 
 def connection_adapter
