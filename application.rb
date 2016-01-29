f5041ca077010fab8c8e1a0578c2ab15cc4597ef
Olivier Lacan <hi@olivierlacan.com>
2016-01-29 09:48:24 -0500

    Hook in Travis API connection logic
    Also Bob is nice.

--- application.rb
+++ application.rb
@@ -2,5 +2,16 @@ require "bundler"
 Bundler.require
 
 get "/" do
-  "Hello World!"
+  "Hi, I'm Bob!"
+end
+
+def travis
+  session = Travis::Pro.session
+  session.api_endpoint = "https://api.travis-ci.com"
+  session.connection = adapter
+  session.github_auth(ENV["GITHUB_API_KEY"])
+end
+
+def connection_adapter
+  Faraday.new { |f| f.adapter :httpclient }
 end
